3932a52101f89ae058dfeab032d3f61ef9289db2
mikeehann <67243995+mikeehann@users.noreply.github.com>
2022-02-27 12:18:52 -0400

    Update ekf_filterpy.py

--- ekf_filterpy.py
+++ ekf_filterpy.py
@@ -1,8 +1,8 @@
 
 from filterpy.kalman import ExtendedKalmanFilter
 from filterpy.common import Q_discrete_white_noise
+import sympy as sp
 import numpy as np
-
 
 '''
 Instantiate an Extended Kalman Filter with 
@@ -13,9 +13,19 @@ Instantiate an Extended Kalman Filter with
     -IMU  x6   (linear acceleration x3)
                 (angular velocity x3)
 
-0 noise properties (u=0)
+2 input properties (u=2)
+    -Velocity
 '''
-kf = ExtendedKalmanFilter(dim_x=3, dim_z=6, dim_u=0)
+class EKF(ExtendedKalmanFilter):
+    def __init__(self, dt, ):
+        ExtendedKalmanFilter.__init__(self, 0.01, )
+        self.dt = dt
+        self.x
+
+        a, x, y, v, w, theta, time = symbols(
+            'a, x, y, v, w, theta, t')
+
+kf = EKF(dim_x=3, dim_z=6, dim_u=0)
 
 # Initial state (location and velocity)
 kf.x = np.array([   [2.],
@@ -48,3 +58,19 @@ while True:
     x = kf.x
     values.append(x)
     plotting_function(x)
+
+
+def getVel(x1, y1, z1, x2, y2, z2, dt):
+    # In m/s
+    vel_x = (x2-x1)/dt
+    vel_y = (y2-y1)/dt
+    vel_z = (z2-z1)/dt
+
+    return (vel_x, vel_y, vel_z)
+
+# Example getVel call
+'''
+vels = getVel(  df.GPS_Long[i+1], df.GPS_Lat[i+1], df.GPS_Alt[i+1],
+                df.GPS_Long[i], df.GPS_Lat[i], df.GPS_Alt[i],
+                dt)
+'''
